c898c7c209c79567dc9006493202833d796f84e8
sergei <osp11011983@mail.ru>
2024-09-25 17:30:52 +0200

    add package data

---
+++ src/test/java/data/APIHelper.java
@@ -0,0 +1,40 @@
+package data;
+
+import com.google.gson.Gson;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
+
+public class APIHelper{
+    private static final RequestSpecification spec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(8080)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    public static void executeRequest(Object requestBody, String endpoint) {
+        Gson gson = new Gson();
+        var body = gson.toJson(requestBody);
+        given()
+                .spec(spec)
+                .body(body)
+                .when()
+                .post(endpoint)
+                .then()
+                .statusCode(200);
+    }
+    public static void executeRequest500(Object requestBody, String endpoint) {
+        Gson gson = new Gson();
+        var body = gson.toJson(requestBody);
+        given()
+                .spec(spec)
+                .body(body)
+                .when()
+                .post(endpoint)
+                .then()
+                .statusCode(500);
+    }
+}
---
+++ src/test/java/data/DataHelper.java
@@ -0,0 +1,82 @@
+package data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataHelper {
+    private static final Faker faker = new Faker(Locale.ENGLISH);
+    private static final Faker fakerWithCyrillicLocale = new Faker(new Locale("ru", "RU"));
+
+    @Value
+    public static class CardData {
+        private final String number;
+        private final String month;
+        private final String year;
+        private final String holder;
+        private final String cvc;
+    }
+    public static CardData getValidApprovedCard() {
+        return new CardData(getNumberByStatus("approved"), generateMonth(1), generateYear(2),
+                generateValidHolder(), generateValidCVC());
+    }
+    public static CardData getValidDeclinedCard() {
+        return new CardData(getNumberByStatus("declined"), generateMonth(1), generateYear(2),
+                generateValidHolder(), generateValidCVC());
+    }
+    public static String getNumberByStatus(String status) {
+        if (status.equalsIgnoreCase("APPROVED")) {
+            return "4444 4444 4444 4441";
+        } else if (status.equalsIgnoreCase("DECLINED")) {
+            return "4444 4444 4444 4442";
+        }
+        return null;
+    }
+    public static String generateValidCardNumberWith13Digits() {
+        return faker.numerify("4444 4444 4444 4");
+    }
+    public static String generateValidCardNumberWith0Digits() {
+        return faker.numerify("0000 0000 0000 0000");
+    }
+    public static String generateMonth(int shiftMonth) {
+        return LocalDate.now().plusMonths(shiftMonth).format(DateTimeFormatter.ofPattern("MM"));
+    }
+    public static String generateYear(int shiftYear) {
+        return LocalDate.now().plusYears(shiftYear).format(DateTimeFormatter.ofPattern("yy"));
+    }
+    public static String generateValidHolder() {
+        return faker.name().fullName().toUpperCase();
+    }
+    public static String generateInvalidHolder() {
+        return faker.name().firstName().toUpperCase() + "-" + "al" + "-"
+                + faker.name().lastName().toUpperCase();
+    }
+    public static String generateInvalidHolderWithCyrillicSymbols() {
+        return fakerWithCyrillicLocale.name().firstName().toUpperCase() + " "
+                + fakerWithCyrillicLocale.name().lastName().toUpperCase();
+    }
+    public static String generateInvalidHolderWithCyrillicSymbols45() {
+        return fakerWithCyrillicLocale.name().firstName().toUpperCase() + 45 + " "
+                + fakerWithCyrillicLocale.name().lastName().toUpperCase() + 45;
+    }
+    public static String generateInvalidHolderFirstNameRuLastEn() {
+        return fakerWithCyrillicLocale.name().firstName().toUpperCase() + faker.name().lastName().toUpperCase();
+    }
+    public static String generateInvalidHolderFirstNameEnLastRu() {
+        return faker.name().firstName().toUpperCase() + fakerWithCyrillicLocale.name().lastName().toUpperCase();
+    }
+    public static String generateInvalidHolderFirstNameEn() {
+        return faker.name().firstName().toUpperCase();
+    }
+    public static String generateValidCVC() {
+        return faker.numerify("###");
+    }
+    public static String generateInvalidCVCWith2Digit() {
+        return faker.numerify("##");
+    }
+    public static String generateRandomOneDigit() {
+        return faker.numerify("#");
+    }
+}
